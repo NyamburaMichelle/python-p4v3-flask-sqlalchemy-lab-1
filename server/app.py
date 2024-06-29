4aea99ff023c1eb6f1d73f12c19eab855f545d91
Michelle <nyamburamichelle400@gmail.com>
2024-06-29 11:11:31 +0300

    completes lab

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 # server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
 
 from models import db, Earthquake
@@ -21,6 +21,43 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+
+@app.route('/earthquakes/<int:id>')
+def get_earthquakes(id):
+    earthquake = Earthquake.query.get(id)
+    if earthquake:
+        response_data = {
+            'id': earthquake.id,
+            'location': earthquake.location,
+            'magnitude': earthquake.magnitude,
+            'year': earthquake.year
+        }
+        return jsonify(response_data), 200
+    else:
+        error_message = {'message': f'Earthquake {id} not found.'}
+        return jsonify(error_message), 404
+
+@app.route('/earthquakes/magnitude/<float:magnitude>', methods=['GET'])
+def get_earthquakes_by_magnitude(magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    
+    # Prepare response data
+    response_data = {
+        'count': len(earthquakes),
+        'quakes': []
+    }
+
+    for earthquake in earthquakes:
+        quake_data = {
+            'id': earthquake.id,
+            'location': earthquake.location,
+            'magnitude': earthquake.magnitude,
+            'year': earthquake.year
+        }
+        response_data['quakes'].append(quake_data)
+
+    return jsonify(response_data), 200
+
 
 
 if __name__ == '__main__':
